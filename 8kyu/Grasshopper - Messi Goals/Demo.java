8c7852a362fccaa531d58e30bd03d4bcff3e190b
Shinjice <Tdna84@gmail.com>
2020-06-01 16:41:48 +0200

    01-06-2020

---
+++ 8kyu/Grasshopper - Messi Goals/Demo.java
@@ -0,0 +1,9 @@
+class Goals {
+  public static int laLigaGoals = 43;
+  public static int championsLeagueGoals = 10;
+  public static int copaDelReyGoals = 5;
+  
+  public static int totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+}
+
+//https://www.codewars.com/kata/55ca77fa094a2af31f00002a/train/java
