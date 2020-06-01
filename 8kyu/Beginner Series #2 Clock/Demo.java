c00acc871a98f59af348291f4d6e0ee4aa643a7a
Shinjice <Tdna84@gmail.com>
2020-06-01 19:58:28 +0200

    Beginner Series #2 Clock

---
+++ 8kyu/Beginner Series #2 Clock/Demo.java
@@ -0,0 +1,8 @@
+public class Clock {
+  public static int Past(int h, int m, int s) {
+    //Happy Coding! ^_^    
+    return 1000 * ((h * 60 * 60) + (m * 60) + (s));
+  }
+}
+
+//https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/java
