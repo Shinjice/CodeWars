e53ff72a9fbe73a563d2d898f82c12ca0d3cdea3
Shinjice <Tdna84@gmail.com>
2020-06-01 16:46:55 +0200

    A+B

---
+++ 8kyu/A + B/Demo.java
@@ -0,0 +1,8 @@
+public class FirstClass {
+    public static byte sum (byte a, byte b) {
+        byte c = (byte) (a + b);
+        return c;
+    }
+}
+
+//https://www.codewars.com/kata/5512a0b0509063e57d0003f5/train/java
