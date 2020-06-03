1f4311a53abd72c8cec4475aa18c5d3331865101
Shinjice <Tdna84@gmail.com>
2020-06-03 21:21:01 +0200

    03-06-2020

---
+++ 8kyu/Number toString/Demo.java
@@ -0,0 +1,6 @@
+public class ToString {
+   static final String  a = Integer.toString(123);
+  
+}
+
+//https://www.codewars.com/kata/53934feec44762736c00044b/train/java
---
+++ 8kyu/is it even/Demo.java
@@ -0,0 +1,7 @@
+public class Number {
+
+  public boolean isEven(double n) {
+    return n % 2 == 0;
+  }
+}
+//https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/java
