1fc281cc67bc06f5097f63336151a20572fc75af
Shinjice <Tdna84@gmail.com>
2020-06-07 20:16:50 +0200

    07 Jun 2020 20:16:50

---
+++ 8kyu/Convert a string to an array/Demo.java
@@ -0,0 +1,9 @@
+public class Solution {
+
+    public static String[] stringToArray(String s) {
+        String[] temp = s.split(" ");
+      return temp;
+    }
+}
+
+//https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/java
