6b0455a4e14b759ef38d493037dc2b01bbb5ab86
Shinjice <Tdna84@gmail.com>
2020-08-11 22:21:38 +0200

    11 Aug 2020 22:21:38

--- 7kyu/lucky number/Demo.java
+++ 7kyu/lucky number/Demo.java
@@ -3,9 +3,9 @@ public class LuckyNumber {
 public static boolean isLucky(long n) {
     long tmp = 0;
     String a = String.valueOf(n);
-    System.out.println(a);
+    //System.out.println(a);
     for(int i = 0; i < a.length(); i++){
-        System.out.println(Integer.valueOf(String.valueOf(a.charAt(i))));
+        //System.out.println(Integer.valueOf(String.valueOf(a.charAt(i))));
         tmp += Integer.valueOf(String.valueOf(a.charAt(i)));
     }
     return tmp % 9 == 0;
