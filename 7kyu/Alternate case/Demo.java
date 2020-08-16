13df9a4be39a674522b4e74f8f3d551fd7d3fda2
Shinjice <Tdna84@gmail.com>
2020-08-16 21:41:59 +0200

    16 Aug 2020 21:41:59

---
+++ 7kyu/Alternate case/Demo.java
@@ -0,0 +1,16 @@
+class Kata {
+    static String alternateCase(final String string) {
+     String strArray[] = string.split("");
+        String tmp = "";
+        for(int i = 0; i<string.length(); i++){
+            if(Character.isUpperCase(string.charAt(i))){
+                tmp+= Character.toLowerCase(string.charAt(i));
+            } else {
+                tmp+= Character.toUpperCase(string.charAt(i));
+            }
+        }
+    return tmp;
+    }
+}
+
+//https://www.codewars.com/kata/57a62154cf1fa5b25200031e/train/java
