006817d8f36fd13e18a1f32606411a3a49637e42
Shinjice <Tdna84@gmail.com>
2020-10-08 13:21:19 +0200

    08 Oct 2020 13:21:19

---
+++ 7kyu/Drying Potatoes/Demo.java
@@ -0,0 +1,8 @@
+class Potatoes {
+    
+    public static int potatoes(int p0, int w0, int p1) {
+        return w0 * (100 - p0) / (100 - p1);
+    }
+}
+
+//https://www.codewars.com/kata/58ce8725c835848ad6000007/train/java
