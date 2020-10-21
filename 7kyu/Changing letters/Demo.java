ef4ed431d385e142d12189ee82894ac02dfd8efc
Shinjice <Tdna84@gmail.com>
2020-10-21 20:24:15 +0200

    21 Oct 2020 20:24:15

---
+++ 7kyu/Changing letters/Demo.java
@@ -0,0 +1,11 @@
+public class Kata {
+    public static String swap(String st){
+      return st.replace("a", "A")
+        .replace("e", "E")
+        .replace("i", "I")
+        .replace("o", "O")
+        .replace("u", "U");
+    }
+}
+
+//https://www.codewars.com/kata/5831c204a31721e2ae000294/train/java
