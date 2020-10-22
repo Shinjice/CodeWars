5e237426aa4b4a3a7b6187f1a95bd8ef9c111e0c
Shinjice <Tdna84@gmail.com>
2020-10-22 19:05:31 +0200

    22 Oct 2020 19:05:31

---
+++ 7kyu/Spacify/Demo.java
@@ -0,0 +1,7 @@
+public class Spacify {
+    public static String spacify(String str){
+        return str.replace("", " ").trim();
+    }
+}
+
+//https://www.codewars.com/kata/57f8ee485cae443c4d000127/train/java
