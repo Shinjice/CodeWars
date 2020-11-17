fce0c4db1e52a259787dee3c2ef8f3acb33e8f44
Shinjice <Tdna84@gmail.com>
2020-11-17 21:15:32 +0100

    17 Nov 2020 21:15:32

---
+++ 7kyu/Covfefe/Demo.java
@@ -0,0 +1,7 @@
+public class Covfefe {
+    public static String covfefe(String tweet) {
+        return tweet.contains("coverage") ? tweet.replace("coverage", "covfefe") : tweet + " covfefe";
+    }
+}
+
+//https://www.codewars.com/kata/592fd8f752ee71ac7e00008a/train/java
