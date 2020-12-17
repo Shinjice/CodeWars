f9ee9ab363400c02e49359a19cced47e6845f1db
Shinjice <Tdna84@gmail.com>
2020-12-17 20:20:18 +0100

    17 Dec 2020 20:20:18

---
+++ 7kyu/Ch4113ng3/Demo.java
@@ -0,0 +1,7 @@
+class Nerd {
+  public static String nerdify(String txt){
+    return txt.replaceAll("[aA]", "4").replaceAll("[eE]", "3").replace("l", "1");
+  }
+}
+
+//https://www.codewars.com/kata/59e9f404fc3c49ab24000112/train/java
