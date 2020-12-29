787aacfbef08ad5b359d4621859021755170eef1
Shinjice <Tdna84@gmail.com>
2020-12-29 16:42:50 +0100

    29 Dec 2020 16:42:50

---
+++ 7kyu/Thinkful - String Drills Repeater/Demo.java
@@ -0,0 +1,7 @@
+public class Repeater{
+  public static String repeat(String string,long n){
+    return string.repeat((int) n);
+  }
+}
+
+//https://www.codewars.com/kata/585a1a227cb58d8d740001c3/train/java
