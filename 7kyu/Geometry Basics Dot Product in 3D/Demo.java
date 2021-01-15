c1889d592c9ea9b58ed4c89c4cd8f16db778a980
Shinjice <Tdna84@gmail.com>
2021-01-15 19:22:53 +0100

    15 Jan 2021 19:22:53

---
+++ 7kyu/Geometry Basics Dot Product in 3D/Demo.java
@@ -0,0 +1,7 @@
+public class GeometryBasics {
+    public static double dotProduct(final Vector3D a, final Vector3D b) {
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+}
+
+//https://www.codewars.com/kata/58e3ea29a33b52c1dc0000c0/train/java
