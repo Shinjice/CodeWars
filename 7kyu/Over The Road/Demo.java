7d506a5fd216f3ba2cf57397eac7713fa2366ed2
Shinjice <Tdna84@gmail.com>
2021-02-17 21:56:05 +0100

    17 Feb 2021 21:56:05

---
+++ 7kyu/Over The Road/Demo.java
@@ -0,0 +1,7 @@
+class CodeWars {
+  public static long overTheRoad(long address, long n) {
+    return 2 * n - --address;
+  }
+}
+
+//https://www.codewars.com/kata/5f0ed36164f2bc00283aed07/train/java
